53a5ba451f2ad084503708f5d78470dbbbfc70d2
ikoyski <christian_sanpedro@yahoo.com>
2025-02-26 07:41:55 +0000

    trying to resolve error when running test

--- src/test/java/com/ikoyski/webtools/psedata/PseDataControllerTest.java
+++ src/test/java/com/ikoyski/webtools/psedata/PseDataControllerTest.java
@@ -6,12 +6,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import com.ikoyski.webtools.psedata.controller.PseDataController;
 import com.ikoyski.webtools.psedata.service.PseDataService;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PseDataControllerTest {
 
 	@Autowired
