5fd2787c12fef7ea56253b3c82ef8d2fc586dc77
ikoyski <christian_sanpedro@yahoo.com>
2025-02-28 17:37:02 +0000

    updated request mappings

--- src/main/java/com/ikoyski/webtools/psedata/controller/PseDataController.java
+++ src/main/java/com/ikoyski/webtools/psedata/controller/PseDataController.java
@@ -3,12 +3,14 @@ package com.ikoyski.webtools.psedata.controller;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ikoyski.webtools.psedata.dto.PseDataResponse;
 import com.ikoyski.webtools.psedata.service.PseDataService;
 
 @RestController
+@RequestMapping("/pse-data")
 public class PseDataController {
 
 	private final PseDataService pseDataService;
@@ -17,14 +19,14 @@ public class PseDataController {
 		this.pseDataService = pseDataService;
 	}
 	
-	@GetMapping(path = "api/v1/stock")
-	@Cacheable(value = "stock")
+	@GetMapping(path = "/v1")
+	@Cacheable(value = "pse-data")
 	public PseDataResponse stock() {
 		return pseDataService.stock();
 	}
 
-	@GetMapping(path = "api/v1/stock/{symbol}")
-	@Cacheable(value = "stock", key = "#symbol")
+	@GetMapping(path = "/v1/{symbol}")
+	@Cacheable(value = "pse-data", key = "#symbol")
 	public PseDataResponse stock(@PathVariable("symbol") String symbol) {
 		return pseDataService.stock(symbol);
 	}
